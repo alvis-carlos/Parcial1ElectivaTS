4189132d929e1a7bc2f2cd92d061e27387e109ec
lpachonda27 <lpachonda@gmail.com>
2021-09-05 02:12:51 -0500

    add my folder

---
+++ Luisa Pachon/src/routes/users.js
@@ -0,0 +1,32 @@
+// Endpoints for external data
+const { Router } = require('express');
+const router = new Router();
+
+const fetch = require('node-fetch');
+
+router.get('/', async (req, res) => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    res.json(data);
+});
+
+router.get('/one', (req, res) => {
+    res.send('Hello World')
+});
+
+router.get('/two', (req, res) => {
+    res.send('Hello Luisa')
+});
+
+router.get('/three', (req, res) => {
+    res.send('Hello Fernando')
+});
+
+// Carlos
+
+router.get('/paises', (req, res) => {
+    const JsonPaises = ['Colombia','Argentina','Mexico','España'];
+    res.json(JsonPaises)
+})
+
+module.exports = router;
